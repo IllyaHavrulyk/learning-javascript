ff0f92cd0277c69955bb7370d47538138c4792b4
Sviatoslav25 <sviatoslavvintoniv@gmail.com>
2020-12-27 01:16:14 +0200

    completed task

---
+++ lesson-if-else/Ex2-JS.js
@@ -0,0 +1,7 @@
+let officialNameJS = prompt("Яка офіційна назва JavaScript");
+if(officialNameJS=="ECMAScript"){
+    alert("Правильно!");
+}
+else {
+    alert("Не знаєте? ECMAScript!");
+}
---
+++ lesson-if-else/Ex3-JS.js
@@ -0,0 +1,10 @@
+let number=prompt("Введіть число");
+if(number<0){
+    alert("-1");
+}
+else if(number>0){
+    alert("1");
+}
+else {
+    alert("0");
+}
